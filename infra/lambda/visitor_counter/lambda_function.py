7f8049f206e5c11d3c95c778193e95f7efb5e8fd
Daniel <dkuan0808@outlook.com>
2024-11-17 18:13:30 +0300

    [WIP] factorise lambda

--- infra/lambda/visitor_counter/lambda_function.py
+++ infra/lambda/visitor_counter/lambda_function.py
@@ -3,77 +3,75 @@ import urllib
 import boto3
 import os
 from botocore.exceptions import ClientError
-# from aws_lambda_powertools import # Logger
+from aws_lambda_powertools import Logger
 from datetime import datetime
+
+# Initialize logger
+logger = Logger()
 
 dynamodb_resource = boto3.resource("dynamodb", region_name="eu-west-1")
 TABLE = dynamodb_resource.Table(os.environ["TABLE"])
 
-def add_visitor_to_table(current_date: str) -> None:
-    try:
-        updates = [
-            # Daily counter
-            {
-                "Key": {"pk": "daily", "sk": current_date},
-                "UpdateExpression": "ADD visitors :inc",
-                "ExpressionAttributeValues": {":inc": 1}
-            },
-            # Total counter
-            {
-                "Key": {"pk": "total", "sk": "historic"},
-                "UpdateExpression": "ADD visitors :inc",
-                "ExpressionAttributeValues": {":inc": 1}
-            }
-        ]
-        
-        for update in updates:
-            TABLE.update_item(**update)
-        
-        # logger.info(">>> DDB Table updated.")
+def add_visitor_to_table(current_date: str) -> str:
+    updates = [
+        # Daily counter
+        {
+            "Key": {"pk": "daily", "sk": current_date},
+            "UpdateExpression": "ADD visitors :inc",
+            "ExpressionAttributeValues": {":inc": 1}
+        },
+        # Total counter
+        {
+            "Key": {"pk": "total", "sk": "historic"},
+            "UpdateExpression": "ADD visitors :inc",
+            "ExpressionAttributeValues": {":inc": 1}
+        }
+    ]
+    
+    for update in updates:
+        TABLE.update_item(**update)
+    
+    logger.info(">>> Visitor added to table.")
 
-        return None
-        
-    except Exception as e:
-        # logger.info(str(e))
-
-        return None
+    return {
+        "statusCode": 200,
+        "headers": headers,
+        "body": json.dumps({
+            "message": ">>> Visitor added to table."
+        })
+    }
 
 
 def get_visitor_count(pk: str, sk: str) -> int | None:
     try:
-        print("#### Getting response")
         response = TABLE.get_item(
             Key={
                 "pk": pk,
                 "sk": sk
             }
         )
-        print(response)
+        logger.info(response)
         
         item = response.get("Item", {})
         visitors = item.get("visitors", 0) 
 
-        print(visitors)
-
-        # logger.info(">>> Visitors retrieved.")
-        # logger.info(visitors)
+        logger.info(">>> Visitors retrieved.")
+        logger.info(visitors)
 
         return visitors
     
     except Exception as e:
-        print(e)
-        # logger.info(str(e))
+        logger.info(str(e))
 
         return None
 
 
 def lambda_handler(event, context):
-    # logger.info(event)
-    print(event)
+    logger.info(event)
 
     headers = {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",  # Or your specific domain
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
     }
@@ -83,24 +81,16 @@ def lambda_handler(event, context):
 
     if "resource" in event and "add_visitor" in event["resource"]:
         try:
-            add_visitor_to_table(current_date=current_date)
-
-            return {
-                "statusCode": 200,
-                "headers": headers,
-                "body": json.dumps({
-                    "message": "Added visitors."
-                })
-            }
+            return add_visitor_to_table(current_date=current_date)
 
         except Exception as e:
-            # logger.info(str(e))
+            logger.info(str(e))
 
             return {
                 "statusCode": 500,
                 "headers": headers,
                 "body": json.dumps({
-                    "message": "Could not add visitor to table."
+                    "message": str(e)
                 })
             }
         
@@ -119,18 +109,17 @@ def lambda_handler(event, context):
             }
 
         except Exception as e:
-            print(e)
-            # logger.info(str(e))
+            logger.info(str(e))
 
             return {
                 "statusCode": 500,
                 "headers": headers,
                 "body": json.dumps({
-                    "message": "Could not get visitor from table."
+                    "message": str(e)
                 })
             }
 
-    # logger.info("Invalid endpoint.")
+    logger.info("Invalid endpoint.")
 
     return {
         "statusCode": 400,
