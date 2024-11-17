d966f32a5491ff886f52eab4737d0345366dda27
Daniel <dkuan0808@outlook.com>
2024-11-17 03:16:37 +0300

    [WIP] Add get_visitor logic

--- infra/lambda/visitor_counter/lambda_function.py
+++ infra/lambda/visitor_counter/lambda_function.py
@@ -9,21 +9,18 @@ from datetime import datetime
 dynamodb_resource = boto3.resource("dynamodb", region_name="eu-west-1")
 TABLE = dynamodb_resource.Table(os.environ["TABLE"])
 
-def add_visitor_to_table() -> None:
+def add_visitor_to_table(current_date: str) -> None:
     try:
-        current_time = datetime.now()
-        current_date = current_time.strftime("%Y-%m-%d")
-
         updates = [
             # Daily counter
             {
-                "Key": {"pk": "daily_", "sk": current_date},
+                "Key": {"pk": "daily", "sk": current_date},
                 "UpdateExpression": "ADD visitors :inc",
                 "ExpressionAttributeValues": {":inc": 1}
             },
             # Total counter
             {
-                "Key": {"pk": "total_", "sk": "historic"},
+                "Key": {"pk": "total", "sk": "historic"},
                 "UpdateExpression": "ADD visitors :inc",
                 "ExpressionAttributeValues": {":inc": 1}
             }
@@ -42,22 +39,22 @@ def add_visitor_to_table() -> None:
         return None
 
 
-def get_visitor_count() -> int | None:
+def get_visitor_count(pk: str, sk: str) -> int | None:
     try:
         response = TABLE.get_item(
             Key={
-                "pk": "total_",
-                "sk": "historic"
+                "pk": pk,
+                "sk": sk
             }
         )
         
         item = response.get("Item", {})
-        total_visitors = item.get("visitors", 0) 
+        visitors = item.get("visitors", 0) 
 
-        logger.info("Total visitors retrieved.")
-        logger.info(total_visitors)
+        logger.info(">>> Visitors retrieved.")
+        logger.info(visitors)
 
-        return total_visitors
+        return visitors
     
     except Exception as e:
         logger.info(str(e))
@@ -68,9 +65,14 @@ def get_visitor_count() -> int | None:
 def lambda_handler(event, context):
     logger.info(event)
 
+    current_time = datetime.now()
+    current_date = current_time.strftime("%Y-%m-%d")
+
     if "resource" in event and "/add_visitor" in event:
         try:
-            add_visitor_to_table()
+            add_visitor_to_table(current_date=current_date)
+
+            return "Added visitors."
 
         except Exception as e:
             logger.info(str(e))
@@ -79,15 +81,16 @@ def lambda_handler(event, context):
         
     elif "resource" in event and "/get_visitor_count" in event:
         try:
-            get_visitor_count()
+            total_visitors = get_visitor_count(pk="total", sk="historic")
+            daily_visitors = get_visitor_count(pk="daily", sk=current_date)
+
+            return total_visitors, daily_visitors
 
         except Exception as e:
             logger.info(str(e))
 
             return "Could not get visitor from table."
 
-    logger.info(event)
+    logger.info("Lambda complete")
 
-    return {
-        "status": "complete"
-    }
+    return "Lambda complete"
