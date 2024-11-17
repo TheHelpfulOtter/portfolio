42a18e35d69d4e540bf77a4615bba3d4d45aafb4
Daniel <dkuan0808@outlook.com>
2024-11-17 14:28:27 +0300

    Fix conditionals for api gw

--- infra/lambda/visitor_counter/lambda_function.py
+++ infra/lambda/visitor_counter/lambda_function.py
@@ -65,32 +65,70 @@ def get_visitor_count(pk: str, sk: str) -> int | None:
 def lambda_handler(event, context):
     logger.info(event)
 
+    headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",  # Or your specific domain
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    }
+
     current_time = datetime.now()
     current_date = current_time.strftime("%Y-%m-%d")
 
-    if "resource" in event and "/add_visitor" in event:
+    if "resource" in event and "add_visitor" in event["resource"]:
         try:
             add_visitor_to_table(current_date=current_date)
 
-            return "Added visitors."
+            return {
+                "statusCode": 200,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "Added visitors."
+                })
+            }
 
         except Exception as e:
             logger.info(str(e))
 
-            return "Could not add visitor to table."
+            return {
+                "statusCode": 500,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "Could not add visitor to table."
+                })
+            }
         
-    elif "resource" in event and "/get_visitor_count" in event:
+    elif "resource" in event and "/get_visitor_count" in event["resource"]:
         try:
             total_visitors = get_visitor_count(pk="total", sk="historic")
             daily_visitors = get_visitor_count(pk="daily", sk=current_date)
 
-            return total_visitors, daily_visitors
+            return {
+                "statusCode": 200,
+                "headers": headers,
+                "body": json.dumps({
+                    "total_visitors": total_visitors,
+                    "daily_visitors": daily_visitors
+                })
+            }
 
         except Exception as e:
             logger.info(str(e))
 
-            return "Could not get visitor from table."
+            return {
+                "statusCode": 500,
+                "headers": headers,
+                "body": json.dumps({
+                    "message": "Could not get visitor from table."
+                })
+            }
 
-    logger.info("Lambda complete")
+    logger.info("Invalid endpoint.")
 
-    return "Lambda complete"
+    return {
+        "statusCode": 400,
+        "headers": headers,
+        "body": json.dumps({
+            "message": "Invalid endpoint"
+        })
+    }
